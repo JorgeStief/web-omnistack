cf3352edde00a4eec126ac87be28366e24c37b9e
Jorge <jorge.stief@gmail.com>
2020-01-18 00:38:18 -0300

    finish

--- src/App.js
+++ src/App.js
@@ -1,48 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
+
 import "./global.css";
 import "./Sidebar.css";
+import "./App.css";
+import "./Main.css";
 
+import DevItem from './components/DevItem/index';
+import DevForm from './components/DevForm/index';
 //Componente: E um bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação
 //Estado: INformações mantidas pelo componente (Lembrar: imutabilidade)
 //Propriedade: Informações que um componente PAI passar para o componente FILHO
+
+
 function App() {
+
+  const [devs, setDevs] = useState([])
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('devs');
+
+      setDevs(response.data);
+    }
+    loadDevs();
+  }, []);
+
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data)
+    setDevs([...devs, response.data]);
+
+  }
   return (
     <div id="app">
       <aside>
         <strong> Cadastrar</strong>
-        <form>
-          <div className="input-block">
-            <label htmlFor="github_username"> Usuário do Github</label>
-            <input name="github_username" id="github_username" required></input>
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias </label>
-            <input name="techs" id="techs" required></input>
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude </label>
-              <input name="latitude" id="latitude" required></input>
-            </div>
-
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude </label>
-              <input name="longitude" id="longitude" required></input>
-            </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev} />
       </aside>
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="" alt=""/>
-            </header>
-          </li>
+          {devs.map(dev => (
+
+            <DevItem key={dev.id} dev={dev} />
+            ))}
         </ul>
 
       </main>
